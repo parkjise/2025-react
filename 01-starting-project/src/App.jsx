575f8d050ca3bd29941cf70e837ba894cc5f9f17
parkjise <monories79@gmail.com>
2025-03-06 23:39:14 +0900

    0306

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -32,19 +32,22 @@ function App() {
 				<section id="core-concepts">
 					<h2>Core Concepts</h2>
 					<ul>
+						{CORE_CONCEPTS.map((concepItem) => (
+							<CoreConcept key={concepItem.title} {...concepItem} />
+						))}
 						{/* <CoreConcept
 							title="Components"
 							description="The core UI building block"
 							image={componentsImg}
 						/> */}
-						<CoreConcept
+						{/* <CoreConcept
 							title={CORE_CONCEPTS[0].title}
 							description={CORE_CONCEPTS[0].description}
 							image={CORE_CONCEPTS[0].image}
 						/>
 						<CoreConcept {...CORE_CONCEPTS[1]} />
 						<CoreConcept {...CORE_CONCEPTS[2]} />
-						<CoreConcept {...CORE_CONCEPTS[3]} />
+						<CoreConcept {...CORE_CONCEPTS[3]} /> */}
 					</ul>
 				</section>
 				<section id="examples">
